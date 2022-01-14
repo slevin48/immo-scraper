bf8491b0e06c1e9e265e9c647c3d28720809a90a
slevin48 <yann.debray@gadz.org>
2022-01-14 08:55:04 +0100

    Test for Orpi Viro

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,26 @@ from bs4 import BeautifulSoup
 import requests as rq
 import pandas as pd
 
-#Century21
-urlbase = 'https://www.century21-adc-meudon.com/'
-page = rq.get(urlbase+'annonces/achat/')
-soup = BeautifulSoup(page.content, "html.parser")
+# #Century21
+# urlbase = 'https://www.century21-adc-meudon.com/'
+# page = rq.get(urlbase+'annonces/achat/')
+# soup = BeautifulSoup(page.content, "html.parser")
 
-prop = soup.find_all(class_="c-the-property-thumbnail-with-content")
-link = [k.a['href'] for k in prop]
-h = soup.find_all(class_="c-text-theme-heading-3 tw-tracking-c21-theme-0 tw-leading-none tw-text-c21-grey-darker")
-title = [k.get_text(strip=True) for k in h]
-p = soup.find_all(class_="c-text-theme-heading-1 is-constant tw-tracking-c21-theme-40 tw-mt-2 tablet-landscape:tw-mt-0")
-price = [int(k.get_text(strip=True).replace(' ', '').replace(u'\xa0€', u'')) for k in p]
+# prop = soup.find_all(class_="c-the-property-thumbnail-with-content")
+# link = [k.a['href'] for k in prop]
+# h = soup.find_all(class_="c-text-theme-heading-3 tw-tracking-c21-theme-0 tw-leading-none tw-text-c21-grey-darker")
+# title = [k.get_text(strip=True) for k in h]
+# p = soup.find_all(class_="c-text-theme-heading-1 is-constant tw-tracking-c21-theme-40 tw-mt-2 tablet-landscape:tw-mt-0")
+# price = [int(k.get_text(strip=True).replace(' ', '').replace(u'\xa0€', u'')) for k in p]
 
-df = pd.DataFrame(zip(title,price,link),columns=["title","price","link"])
+# df = pd.DataFrame(zip(title,price,link),columns=["title","price","link"])
 
-st.markdown('## Century 21')
-df
+# st.markdown('## Century 21')
+# df
 
-#Orpi
+#Orpi Viro
 urlorpi = 'https://www.orpi.com/'
-page = rq.get(urlorpi+'agenceorangerie/acheter')
+page = rq.get(urlorpi+'viroflay/acheter')
 soup = BeautifulSoup(page.content, "html.parser")
 h = soup.find_all(class_="u-link-unstyled c-overlay__link")
 title = [k.get_text(strip=True) for k in h]
